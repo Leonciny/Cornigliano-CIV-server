5e02bdd9703298fa4a34154629c8913bc448fdc8
Leonciny <albeleo75@gmail.com>
2023-02-21 13:09:25 +0100

    updated index

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const process_1 = __importDefault(require("process"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config({ path: `${__dirname}/.env` });
+const app = (0, express_1.default)(), PORT = process_1.default.env.PORT;
+app.use((0, cors_1.default)());
+app.use(body_parser_1.default.json());
+app.listen(PORT, function () {
+    console.log(`Server listening on port ${PORT}`);
+});
+console.log();
--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
-import process from "process";
-import fs from "fs"
+import express from "express"
+import process from "process"
 import dotenv from "dotenv"
+import bodyParser from "body-parser";
+import cors from "cors"
 
 dotenv.config({path : `${__dirname}/.env`})
 
-process.env
+const app = express(),
+      PORT = process.env.PORT
+
+app.use(cors());
+app.use(bodyParser.json());
+app.listen(PORT, function () {
+	console.log(`Server listening on port ${PORT}`);
+})
+console.log()
