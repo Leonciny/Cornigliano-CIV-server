dede37bffeec7185bd315729062e8923288ae264
SastheSus <michele.bardotti04@gmail.com>
2023-02-23 12:01:47 +0100

    update queries

--- src/database/queries.ts
+++ src/database/queries.ts
@@ -1,12 +1,22 @@
 const PARAM_ID = "?";
 
 const dictQuery = {
-    'login'               : "Query login",
-    'findUtente'          : "Query findUtente",
-    'addUtente'           : "Query addUtente",
-    'showNegoziActive'    : "Query showNegoziActive",
-    'showNegoziNotActive' : "Query showNegoziNotActive",
-    'showAllNegozi'       : "SELECT id,nome,indirizzo FROM Negozio"
+    'login'               : "SELECT id FROM Utente WHERE (username="+ PARAM_ID +"OR email="+ PARAM_ID +") AND password="+ PARAM_ID,
+    'findUtente'          : "SELECT id FROM Utente WHERE email="+ PARAM_ID +" OR username="+ PARAM_ID,
+    'showUtente'          : "SELECT nome, username, punti FROM Utente WHERE id="+PARAM_ID,
+    'addUtente'           : "INSERT INTO Utente(email,password,data_iscr,username,nome) VALUES("+PARAM_ID+","+PARAM_ID+","+PARAM_ID+","+PARAM_ID+","+PARAM_ID+")",
+    'showAllPremi'        : "SELECT id, nome FROM Premio",
+    'showPremio'          : "SELECT nome,descrizione,punti_nessari FROM Premio WHERE id="+ PARAM_ID,
+    'addPremio'           : "INSERT INTO Premio(nome,descrizione,punti_necessari,quant_magazzino) VALUES("+PARAM_ID+","+PARAM_ID+","+PARAM_ID+","+PARAM_ID+")",
+    'showNegoziActive'    : "SELECT id, nome, indirizzo FROM Negozio WHERE is_active=true",
+    'showNegoziNotActive' : "SELECT id, nome, indirizzo FROM Negozio WHERE is_active=false",
+    'showNegozio'         : "SELECT nome, descrizione, indirizzo, coordinate FROM Negozio WHERE id="+ PARAM_ID,
+    'showAllNegozi'       : "SELECT id ,nome, indirizzo FROM Negozio",
+    'addNegozio'          : "INSERT INTO Negozio(nome,data_registrazione,is_active,descrizione,indirizzo,coordinate) VALUES("+PARAM_ID+","+PARAM_ID+", true,"+PARAM_ID+","+PARAM_ID+","+PARAM_ID+")",
+    'addAcquisto'         : "INSERT INTO Acquisto(data_ora,punti,idMegozio,idUtente) VALUES("+PARAM_ID+","+PARAM_ID+","+PARAM_ID+","+PARAM_ID+")",
+    'addPunti'            : "UPDATE Utente SET punti = (SELECT punti FROM Utente WHERE id="+PARAM_ID+")+"+PARAM_ID+"WHERE id="+PARAM_ID,
+    'redeemPremio'        : "INSERT INTO UtenteRiscattaPremio(idUtente,idPremio,is_reedemed) VALUES("+PARAM_ID+","+PARAM_ID+",true)",
+    'removePunti'         : "UPDATE Utente SET punti = (SELECT punti FROM Utente WHERE id="+PARAM_ID+")-(SELECT punti_necessari FROM Premio WHERE id="+PARAM_ID+")"
 }
 
 function useQueries(...parameters: Array<string>){
@@ -33,39 +43,16 @@ function useQueries(...parameters: Array<string>){
                         break
         case "showNegoziActive": if(parameters.length != 1)
                                     return "ERROR: Incorrect number of parameters. NOT required: 0"
-                                dictQuery.showNegoziActive.replace(PARAM_ID,"true")
+                                return dictQuery.showNegoziActive
                                 break
         case "showNegoziNotActive": if(parameters.length != 1)
                                         return "ERROR: Incorrect number of parameters. NOT required: 0"
-                                    dictQuery.showNegoziActive.replace(PARAM_ID,"false")
+                                    return dictQuery.showNegoziNotActive
                                     break
         case "showNegozio": if(parameters.length != 2)
                                 return "ERROR: Incorrect number of parameters. Required only ID: 1"
-                            dictQuery.showNegoziNotActive.replace(PARAM_ID,parameters[1])
+                            dictQuery.showNegozio.replace(PARAM_ID,parameters[1])
                             break
-        case "":;
     }
-
 }
 export default useQueries()
-let queries = {
-    'login'         : "SELECT id FROM Utente WHERE (username="+ PARAM_ID +"OR email="+ PARAM_ID +") AND password="+ PARAM_ID,
-    'findUtente'    : "SELECT id FROM Utente WHERE email="+ PARAM_ID +" OR username="+ PARAM_ID,
-    'showUtente'    : "SELECT nome, username, punti FROM Utente WHERE id="+PARAM_ID,
-    'addUtente'     : "INSERT INTO Utente(email,password,data_iscr,username,nome) VALUES("+PARAM_ID+","+PARAM_ID+","+PARAM_ID+","+PARAM_ID+","+PARAM_ID+")",
-    'showAllPremi'  : "SELECT id, nome FROM Premio",
-    'showPremio'    : "SELECT nome,descrizione,punti_nessari FROM Premio WHERE id="+ PARAM_ID,
-    'addPremio'     : "INSERT INTO Premio(nome,descrizione,punti_necessari,quant_magazzino) VALUES("+PARAM_ID+","+PARAM_ID+","+PARAM_ID+","+PARAM_ID+")",
-    'showAllNegozi' : "SELECT id, nome FROM Negozio WHERE is_active=true",
-    'showNegozio'   : "SELECT nome, descrizione, indirizzo, coordinate FROM Negozio WHERE id="+ PARAM_ID,
-    'addNegozio'    : "INSERT INTO Negozio(nome,data_registrazione,is_active,descrizione,indirizzo,coordinate) VALUES("+PARAM_ID+","+PARAM_ID+", true,"+PARAM_ID+","+PARAM_ID+","+PARAM_ID+")",
-    'addAcquisto'   : "INSERT INTO Acquisto(data_ora,punti,idMegozio,idUtente) VALUES("+PARAM_ID+","+PARAM_ID+","+PARAM_ID+","+PARAM_ID+")",
-    'addPunti'      : "UPDATE Utente SET punti = (SELECT punti FROM Utente WHERE id="+PARAM_ID+")+"+PARAM_ID+"WHERE id="+PARAM_ID,
-    'redeemPremio'  : "INSERT INTO UtenteRiscattaPremio(idUtente,idPremio,is_reedemed) VALUES("+PARAM_ID+","+PARAM_ID+",true)",
-    'removePunti'   : "UPDATE Utente SET punti = (SELECT punti FROM Utente WHERE id="+PARAM_ID+")-(SELECT punti_necessari FROM Premio WHERE id="+PARAM_ID+")"
-}
-/*
-queries.login.replace(PARAM_ID, param1)
-queries.login.replace(PARAM_ID, param2)
-queries.login.replace(PARAM_ID, param3)
-*/
